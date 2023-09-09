509134d16b527e2f861e656cae7361d758d380f8
Vivek  Rajpoot <vivekrajput2515@gmail.com>
2023-09-09 16:25:01 +0000

    data-types

---
+++ js-basics/data-types.js
@@ -0,0 +1,18 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("hello"), alert(3+3) // we are using nodejs, not the browser
+
+let name = "Vivek Rajpoot" // string
+let age = 18 // number  -> range 2 to power 53
+let isLoggedIn = true // boolean
+
+// null -> standalone value (representation of empty value)
+// undefined-> value is not assigned to the datatype
+// symbol -> used for the uniqueness
+
+//object
+
+// typeof
+console.log(typeof null)  // showing it as object
+console.log(typeof undefined) // but it is showing same as undefined
+
