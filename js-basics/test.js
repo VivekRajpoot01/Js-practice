86c2c1ca1db3e6db1cd2f8f6ff5e37e3b44cffa5
Vivek  Rajpoot <vivekrajput2515@gmail.com>
2023-09-09 16:06:10 +0000

    commited

--- js-basics/test.js
+++ js-basics/test.js
@@ -1,1 +1,18 @@
-console.log("Hii, this is Vivek Rajpoot this side")
+//console.log("Hii, this is Vivek Rajpoot this side")
+
+const accountId = 144553
+let accountEmail = "vivekrajput2515@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jalandhar"
+
+let accountState
+//accountId = 2
+accountEmail = "vivraj@gmail..com"
+accountPassword = "12232"
+accountCity = "Phagwara"
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+/*
+Prefer not to use var
+beacause of issue in block issue and functional scope
+*/
